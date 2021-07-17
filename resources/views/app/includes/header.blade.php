a8c8879ffa621caa931a2b786b9215eec4ef4239
Thalles Rangel Lopes <thalles.lopes@picpay.com>
2021-07-17 14:44:53 -0300

    só permiti exibir corporate name caso validar

--- resources/views/app/includes/header.blade.php
+++ resources/views/app/includes/header.blade.php
@@ -1,1 +1,3 @@
-<button type="button" class="btn btn-outline-success">{{ session('collaborator.corporate_name') }}</button>
+@if(!in_array(session('collaborator.role'), ['analista', 'assistente'], true))
+    <a href="{{ route('unity.list') }}" class="btn btn-outline-success">{{ session('collaborator.corporate_name') }}</a>
+@endif
