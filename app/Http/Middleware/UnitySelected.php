a5ba86f22875950f7fe792c953760a6beb1450ac
Thalles <87503581+thallesvilaapps@users.noreply.github.com>
2021-08-04 14:45:41 -0300

    clean coee

--- app/Http/Middleware/UnitySelected.php
+++ app/Http/Middleware/UnitySelected.php
@@ -3,14 +3,12 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use Session;
 
 class UnitySelected
 {
     public function handle($request, Closure $next)
     {
-
-        if (in_array(session('collaborator.role'), [ 'analista', 'assistente' ]) && !session('collaborator.id_unity') ) {
+        if (in_array(session('collaborator.role'), [ 'analista', 'assistente' ]) && !session('collaborator.id_unity')) {
             session()->flush();
             return redirect(route('login.signIn'))->with('not_found_id_unity', 'Usuário sem unidade atribuída, contatar o administrador do sistema');
         }
